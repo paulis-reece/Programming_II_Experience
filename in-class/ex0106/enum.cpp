2deae34992e08685f942c7dff3381a0fc25abf6a
James Foster <github@jgfoster.net>
2020-01-06 10:36:16 -0800

    Update with enum exercise.

---
+++ in-class/ex0106/enum.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+ *
+ * In-Class Exercise: Enumeration Data Types
+ *
+ * File Name: enum.cpp
+ * Username:  ?
+ * Username:  ?
+ * Course:    CPTR 142
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   enum City {CITY_SEATTLE, CITY_SPOKANE, CITY_PENDLETON, CITY_PORTLAND};
+   City myCity;
+
+   myCity = CITY_SPOKANE;
+
+   /* Print "OR", "WA", or "??" based on location of myCity  */
+
+   return 0;
+}
