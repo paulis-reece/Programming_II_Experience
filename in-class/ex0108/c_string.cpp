24ac1ac3b8ec0b642d435829014baeedc0acf98c
James Foster <github@jgfoster.net>
2020-01-07 20:25:02 -0800

    Add exercises for enum and c_string

---
+++ in-class/ex0108/c_string.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+ *
+ * In-Class Exercise: C-Strings
+ *
+ * File Name: c_string.cpp
+ * Username:  ?
+ * Username:  ?
+ * Course:    CPTR 142
+ */
+
+#include <iostream> // for cin and cout
+using namespace std;
+
+
+/*====================================================================
+ * Main program
+ */
+int main() {
+
+    // Initialize a string with a size
+
+    // Initialize a string without a size
+
+    // Print each string to the console
+
+    // Change characters
+
+    // Print the changed name
+
+    // Print out a string vertically. (Each character on a new line)
+
+    return 0;
+}
+
