3879373e357158857dab416268ce2bd29262183b
reklre <reklre@cs.wallawalla.edu>
2020-01-08 21:49:52 +0000

    autosave

--- in-class/ex0108/c_string.cpp
+++ in-class/ex0108/c_string.cpp
@@ -3,32 +3,43 @@
  * In-Class Exercise: C-Strings
  *
  * File Name: c_string.cpp
- * Username:  ?
- * Username:  ?
+ * Username:  reklre
+ * Username:  pricqu
  * Course:    CPTR 142
  */
 
 #include <iostream> // for cin and cout
 using namespace std;
 
-
 /*====================================================================
  * Main program
  */
 int main() {
+  int i = 0;
 
-    // Initialize a string with a size
+  // Initialize a string with a size
+  char size[10] = "Kira";
 
-    // Initialize a string without a size
+  // Initialize a string without a size
+  char unknownSize[] = "Jace";
 
-    // Print each string to the console
+  // Print each string to the console
+  cout << size << endl;
+  cout << unknownSize << endl;
 
-    // Change characters
+  // Change characters
+  size[0] = 'S';
+  unknownSize[2] = 'k';
+  // Print the changed name
+  cout << size << " ";
+  cout << unknownSize;
+  cout << endl;
 
-    // Print the changed name
+  // Print out a string vertically. (Each character on a new line)
+  while (size[i] != '\0') {
+    cout << size[i] << endl;
+    i++;
+  }
 
-    // Print out a string vertically. (Each character on a new line)
-
-    return 0;
+  return 0;
 }
-
--- in-class/ex0108/enum.cpp
+++ in-class/ex0108/enum.cpp
@@ -3,8 +3,8 @@
  * In-Class Exercise: Enumeration Data Types
  *
  * File Name: enum.cpp
- * Username:  ?
- * Username:  ?
+ * Username:  reklre
+ * Username:  pricqu
  * Course:    CPTR 142
  */
 
@@ -18,6 +18,27 @@ int main() {
    myCity = CITY_SPOKANE;
 
    /* Print "OR", "WA", or "??" based on location of myCity  */
+  if(myCity == CITY_SEATTLE || myCity == CITY_SPOKANE){
+      cout << "WA" << endl;
+  } else if(myCity == CITY_PENDLETON || myCity == CITY_PORTLAND){
+      cout << "OR" << endl;
+  } else {
+      cout << "??" << endl;
+  }
 
+
+
+  switch(myCity){
+      case CITY_SEATTLE:
+      case CITY_SPOKANE:
+      cout << "WA" << endl;
+      break;
+      case CITY_PENDLETON:
+      case CITY_PORTLAND:
+      cout << "OR" << endl;
+      break;
+      default:
+      cout << "??" << endl;
+  }
    return 0;
 }
