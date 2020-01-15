ebb944ca78dde435e40a7ff36c0eb6fc651f474d
reklre <reklre@cs.wallawalla.edu>
2020-01-15 22:21:37 +0000

    autosave

--- in-class/ex0115/car.cpp
+++ in-class/ex0115/car.cpp
@@ -3,7 +3,7 @@
  * In-Class Exercise: Class definition
  *
  * File Name: car.cpp
- * Username:  ?
+ * Username:  reklre
  * Username:  ?
  * Course:    CPTR 142
  *
@@ -15,39 +15,38 @@ using namespace std;
 // https://en.wikipedia.org/wiki/List_of_counties_in_Washington
 enum CountyFIPS { FIPS_ADAMS = 1, FIPS_KING = 33, FIPS_WALLA_WALLA = 71 };
 
-class CarRegistration{
-	public:
-		void	setYear(int newYear);
-		void	setCounty(CountyFIPS newCounty);
-		void	setCode(int newCode);
-		void	printLicenseNum();
-	private:
-		int			year;
-		CountyFIPS	county;
-		int			code;
+class CarRegistration {
+public:
+  void setYear(int newYear);
+  void setCounty(CountyFIPS newCounty);
+  void setCode(int newCode);
+  void printLicenseNum();
+
+private:
+  int year;
+  CountyFIPS county;
+  int code;
 };
 
-void CarRegistration::setYear(int newYear) {
-	year = newYear;
-}
+void CarRegistration::setYear(int newYear) { year = newYear; }
 
-void CarRegistration::setCounty(CountyFIPS newCounty) {
-	county = newCounty;
-}
+void CarRegistration::setCounty(CountyFIPS newCounty) { county = newCounty; }
 
-void CarRegistration::setCode(int newCode) {
-	code = newCode;
-}
+void CarRegistration::setCode(int newCode) { code = newCode; }
 
 // PrintLicenseNum() with format <county>-<year>-<code>
-
+void CarRegistration::printLicenseNum() {
+  cout << county << "-";
+  cout << year << "-";
+  cout << code << endl;
+}
 int main() {
-   CarRegistration car;
+  CarRegistration car;
 
-   car.setYear(2019);
-   car.setCounty(FIPS_WALLA_WALLA);
-   car.setCode(42);
-   car.printLicenseNum();
+  car.setYear(2019);
+  car.setCounty(FIPS_WALLA_WALLA);
+  car.setCode(42);
+  car.printLicenseNum();
 
-   return 0;
+  return 0;
 }
--- in-class/ex0115/class.cpp
+++ in-class/ex0115/class.cpp
@@ -3,57 +3,52 @@
  * In-Class Exercise: Classes
  *
  * File Name: class.cpp
- * Username:  ?
+ * Username:  reklre
  * Username:  ?
  * Course:    CPTR 142
  */
 
-#include <iostream> 	// cin and cout
-#include <math.h>       // pow
+#include <iostream> // cin and cout
+#include <math.h>   // pow
 
 using namespace std;
 
 class Point {
-	public:
-		void setX(int newX);
-		void setY(int newY);
-		double distanceFromOrigin();
-	private:
-		int x, y;
+public:
+  void setX(int newX);
+  void setY(int newY);
+  double distanceFromOrigin();
+
+private:
+  int x, y;
 };
 
 // main entry point
 int main() {
-	// use the new struct
-	Point target;
-	int x, y;
-	double distance;
-	cout << "Enter x coordinate: ";
-	cin >> x;
-	cout << "Enter y coordinate: ";
-	cin >> y;
+  // use the new struct
+  Point target;
+  int x, y;
+  double distance;
+  cout << "Enter x coordinate: ";
+  cin >> x;
+  cout << "Enter y coordinate: ";
+  cin >> y;
 
-	// TODO: assign values
+  // TODO: assign values
+  target.setX(x);
+  target.setY(y);
+  // TODO: obtain distance
+  distance = target.distanceFromOrigin();
 
-	// TODO: obtain distance
-	distance = 0;
+  cout << "target is " << distance << " units from origin" << endl;
 
-	cout << "target is " << distance << " units from origin" << endl;
-
-    return 0;
+  return 0;
 }
-
 
 //	Class implementation comes later!
 
-void Point::setX(int newX) {
-	x = newX;
-}
+void Point::setX(int newX) { x = newX; }
 
-void Point::setY(int newY) {
-	y = newY;
-}
+void Point::setY(int newY) { y = newY; }
 
-double Point::distanceFromOrigin() {
-	return sqrt(pow(x, 2) + pow(y, 2));
-}
+double Point::distanceFromOrigin() { return sqrt(pow(x, 2) + pow(y, 2)); }
