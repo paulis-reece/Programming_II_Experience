d1cbdc27dab47a1676fd3a4e8562c2887849761f
James Foster <github@jgfoster.net>
2020-01-15 19:25:34 -0800

    Exercises for 16-Jan-2020.

---
+++ in-class/ex0116/car.cpp
@@ -0,0 +1,55 @@
+/*************************************************************************
+ *
+ * In-Class Exercise: Class definition
+ *
+ * File Name: car.cpp
+ * Username:  ?
+ * Username:  ?
+ * Course:    CPTR 142
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+// https://en.wikipedia.org/wiki/List_of_counties_in_Washington
+enum CountyFIPS { FIPS_ADAMS = 1, FIPS_KING = 33, FIPS_WALLA_WALLA = 71 };
+
+class CarRegistration{
+	public:
+		void	setYear(int newYear);
+		void	setCounty(CountyFIPS newCounty);
+		void	setCode(int newCode);
+		void	printLicenseNum();
+	private:
+		int			year;
+		CountyFIPS	county;
+		int			code;
+};
+
+void CarRegistration::setYear(int newYear) {
+	year = newYear;
+}
+
+void CarRegistration::setCounty(CountyFIPS newCounty) {
+	county = newCounty;
+}
+
+void CarRegistration::setCode(int newCode) {
+	code = newCode;
+}
+
+void CarRegistration::printLicenseNum() {
+	cout << county << "-" << year << "-" << code << endl;
+}
+
+int main() {
+   CarRegistration car;
+
+   car.setYear(2019);
+   car.setCounty(FIPS_WALLA_WALLA);
+   car.setCode(42);
+   car.printLicenseNum();
+
+   return 0;
+}
