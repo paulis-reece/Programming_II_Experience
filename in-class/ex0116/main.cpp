7944b9883571ec7511fbd0a46c4f4176bd4caafb
reklre <reklre@cs.wallawalla.edu>
2020-01-16 22:25:48 +0000

    autosave

--- in-class/ex0116/main.cpp
+++ in-class/ex0116/main.cpp
@@ -3,14 +3,14 @@
  * In-Class Exercise: Creating a Class
  *
  * File Name: main.cpp
- * Username:  ?
+ * Username:  reklre
  * Username:  ?
  * Course:    CPTR 142
  *
  */
 
-#include <iostream> 	// for cin and cout
-#include <assert.h>		// assert()
+#include <assert.h> // assert()
+#include <iostream> // for cin and cout
 using namespace std;
 
 /*
@@ -21,13 +21,22 @@ using namespace std;
   -  include a member function that outputs the current value
   -  ensure that the value never becomes negative
 */
-
+class CounterType {
+public:
+  void setCounter(int count) { Counter = count; }
+  void Counting() { Counter++; }
+  void DecreaseCount() { Counter--;}
+unsigned getCount(){return Counter;}
+void printCount(){}
+private:
+  unsigned int Counter;
+};
 /*====================================================================
  * Main program
  */
 int main() {
 
-    cout << "Counter Checkpoint" << endl;
+  cout << "Counter Checkpoint" << endl;
 
-    return 0;
+  return 0;
 }
