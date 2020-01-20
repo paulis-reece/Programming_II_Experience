cb8e1bb4b60837cb3b622d73898db64bbf7c40d6
reklre <reklre@cs.wallawalla.edu>
2020-01-20 03:26:51 +0000

    autosave

--- homework/03_structs/points.cpp
+++ homework/03_structs/points.cpp
@@ -3,43 +3,55 @@
  * Homework Assignment: struct for polar and cartesian points
  *
  * File Name:       points.cpp
- * Name:            ?
+ * Name:            reklre
  * Course:          CPTR 142
  * Date:            January 17, 2020
  *
  */
 
+#include <cmath>    // for trig functions
 #include <iostream> // for cin and cout
-#include <math.h>   // for trig functions
 using namespace std;
 
 #include "points.h"
 
-void CartesianToPolar(double x, double y, double &radius, double &theta) {
-  radius = sqrt(pow(x, 2) + pow(y, 2));
-  theta = atan2(y, x);
+struct CartesianPoint {
+  double x;
+  double y;
+  double x2;
+  double y2;
+};
+CartesianPoint group;
+CartesianPoint group1;
+struct PolarPoint {
+  double radius;
+  double theta;
+};
+PolarPoint nextGroup;
+void CartesianToPolar(CartesianPoint) {
+  nextGroup.radius = sqrt(pow(group.x, 2) + pow(group.y, 2));
+  nextGroup.theta = atan2(group.y, group.x);
 }
 
-void PolarToCartesian(double radius, double theta, double &x, double &y) {
-  x = radius * cos(theta);
-  y = radius * sin(theta);
+void PolarToCartesian(PolarPoint) {
+  group.x = nextGroup.radius * cos(nextGroup.theta);
+  group.y = nextGroup.radius * sin(nextGroup.theta);
 }
 
-double LengthC(double x, double y) { return (sqrt(pow(x, 2.0) + pow(y, 2.0))); }
+double LengthC(CartesianPoint) { return (sqrt(pow(group.x, 2.0) + pow(group.y, 2.0))); }
 
-void NormalizedC(double xIn, double yIn, double &xOut, double &yOut) {
-  double length = LengthC(xIn, yIn);
-  xOut = xIn / length;
-  yOut = yIn / length;
+void NormalizedC(CartesianPoint) {
+  double length = LengthC(group.x2, group.y2);
+  group.x2 = group.x2 / length;
+  group.y2 = group.y2 / length;
 }
 
-double DotProductC(double firstX, double firstY, double secondX,
-                   double secondY) {
-  return (firstX * secondX + firstY * secondY);
+double DotProductC(CartesianPoint) {
+  return (group.x * group1.x + group.y * group1.y);
 }
 
 void SumC(double firstX, double firstY, double secondX, double secondY,
           double &resultX, double &resultY) {
-  resultX = firstX + secondX;
-  resultY = firstY + secondY;
+  group1.x2 = group.x + group1.x;
+  group1.y2 = group.x + group1.y;
 }
