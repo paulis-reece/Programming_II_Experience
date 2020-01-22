8f21c49bf490b00407134450cd6c08da512db732
reklre <reklre@cs.wallawalla.edu>
2020-01-22 21:26:30 +0000

    autosave

--- Fun Times/Test.cpp
+++ Fun Times/Test.cpp
@@ -6,40 +6,58 @@
 using namespace std;
 
 int main() {
- string trash;
+  string trash;
   char firstName[100];
   char lastName[100];
-  char game[50] = "YES   GGG     Doc       tog1222!!!@@@";
-int counter = 0;
-int count = 0;
-int counting = 0;
-int temp = 0;
-  for(int r = 0; game[r] != '\0'; r++){
-if(isspace(game[r])){
+  char game[50] = "Dave";
+  char chess[50] = "evad";
+  int counter = 0;
+  int count = 0;
+  int counting = 0;
+  int temp = 0;
+  for (int r = 0; game[r] != '\0'; r++) {
+    if (isspace(game[r])) {
 
-count++;
-} else if(isblank(game[r])){
+      count++;
+    } else if (isblank(game[r])) {
 
-count++;
-} else if(ispunct(game[r])){
+      count++;
+    } else if (ispunct(game[r])) {
 
-count++;
-} else if(iscntrl(game[r])){
+      count++;
+    } else if (iscntrl(game[r])) {
 
-count++;
-} else if(tolower(game[r])){
-firstName[r - count] = tolower(game[r]);
-}
+      count++;
+    } else if (tolower(game[r])) {
+      firstName[r - count] = tolower(game[r]);
+    }
 
-
-  counter++;
+    counter++;
   }
   firstName[counter] = '\0';
-cout << firstName << endl;
-for(int c = 0; game[c] != '\0'; c++){
-lastName[c] = firstName[counter];
-counter--;
-}
-cout << lastName << endl;
+  cout << firstName << endl;
+
+count = 0;
+counter = 0;
+   for (int r = 0; chess[r] != '\0'; r++) {
+    if (isspace(chess[r])) {
+
+      count++;
+    } else if (isblank(game[r])) {
+
+      count++;
+    } else if (ispunct(game[r])) {
+
+      count++;
+    } else if (iscntrl(game[r])) {
+
+      count++;
+    } else if (tolower(game[r])) {
+      lastName[r - count] = tolower(game[r]);
+    }
+
+    counter++;
+  }
+
   return 0;
 }
