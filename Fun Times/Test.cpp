98d46a671931d600404e4f60a1fd04b02bc15628
reklre <reklre@cs.wallawalla.edu>
2020-01-23 01:06:16 +0000

    autosave

--- Fun Times/Test.cpp
+++ Fun Times/Test.cpp
@@ -3,18 +3,22 @@
 #include <cstdlib> // for exit
 #include <cstring>
 #include <iostream> // for cin and cout
+#include<vector>
 using namespace std;
 
 int main() {
   string trash;
+  bool verdict;
   char firstName[100];
   char lastName[100];
+  vector<string> name;
   char game[50] = "Dave";
   char chess[50] = "evad";
   int counter = 0;
   int count = 0;
   int counting = 0;
   int temp = 0;
+  
   for (int r = 0; game[r] != '\0'; r++) {
     if (isspace(game[r])) {
 
@@ -37,28 +41,29 @@ int main() {
   firstName[counter] = '\0';
   cout << firstName << endl;
 
-count = 0;
-counter = 0;
-   for (int c = 0; chess[c] != '\0'; c++) {
+  count = 0;
+  counter = 0;
+  for (int c = 0; chess[c] != '\0'; c++) {
     if (isspace(chess[c])) {
 
       count++;
-    } else if (isblank(game[c])) {
+    } else if (isblank(chess[c])) {
 
       count++;
-    } else if (ispunct(game[c])) {
+    } else if (ispunct(chess[c])) {
 
       count++;
-    } else if (iscntrl(game[c])) {
+    } else if (iscntrl(chess[c])) {
 
       count++;
-    } else if (tolower(game[c])) {
-      lastName[c - count] = tolower(game[c]);
+    } else if (tolower(chess[c])) {
+      name[c].push_back(tolower(chess[c]));
     }
-
     counter++;
   }
-  lastName[counter] = '\0';
-  cout << lastName << endl;
+  for(int i = 0; i < name.size(); i++){
+      
+  }
+  
   return 0;
 }
