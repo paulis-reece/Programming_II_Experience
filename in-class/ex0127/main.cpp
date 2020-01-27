80ec75e4a43220686ae59a34eec1b9ab5468b0c6
James Foster <github@jgfoster.net>
2020-01-27 08:52:20 -0800

    Exercise for 27-January-2020.

---
+++ in-class/ex0127/main.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+ *
+ * Checkpoint Assignment: Inheritance
+ *
+ * File Name:   main.cpp
+ * Username:  	?
+ * Username:  	?
+ * Course:      CPTR 142
+ * Date:        20 Feb 2019
+ *
+ */
+#include <iostream>
+#include "Person.h"
+using namespace std;
+
+
+int main() {
+
+    Person citizen;
+    citizen.setName("Joe");
+    citizen.think();
+
+    return 0;
+}
