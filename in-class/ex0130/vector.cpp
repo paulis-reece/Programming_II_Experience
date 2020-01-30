375664b972299d8d789db433a8e868c242679187
reklre <reklre@cs.wallawalla.edu>
2020-01-30 21:52:40 +0000

    autosave

--- in-class/ex0130/vector.cpp
+++ in-class/ex0130/vector.cpp
@@ -3,25 +3,26 @@
  * In-Class Exercise: Vector
  *
  * File Name: vector.cpp
- * Username:  ?
+ * Username:  reklre
  * Username:  ?
  * Course:    CPTR 142
  *
  */
 
-#include <iostream> 	// for cin and cout
-#include <vector>	  	// vector
+#include <iostream> // for cin and cout
+#include <vector>   // vector
 using namespace std;
 
 class State {
-  public:
-    State(string name, int population);
-    string getName() const { return name; }
-    int getPopulation() const { return population; }
-    void setPopulation(int population) { this->population = population; }
-  private:
-    string name;
-    int population; // in thousands
+public:
+  State(string name, int population);
+  string getName() const { return name; }
+  int getPopulation() const { return population; }
+  void setPopulation(int population) { this->population = population; }
+
+private:
+  string name;
+  int population; // in thousands
 };
 
 // constructor
@@ -32,15 +33,25 @@ State::State(string name, int population) {
 
 int main() {
   // Exrcise 1: define a vector and print its size
-
+  vector<State> state;
+  cout << "Size: " << state.size() << endl;
   // Exercise 2: add elements
-
+  State wa("Washington", 7614893);
+  state.push_back(wa);
+  state.push_back(State("Oregon", 4217737));
   // Exercise 3: loop and print
   cout << "Here are the elements: " << endl;
-
+  for (auto states : state) {
+    cout << states.getName() << endl;
+    cout << states.getPopulation() << endl;
+  }
   // Exercise 4: project 5% growth and print the list again
-
   cout << endl << "After adjustment, here are the elements: " << endl;
+  for (auto &states : state) {
+    states.setPopulation(states.getPopulation() * 1.05);
+    cout << states.getName() << endl;
+    cout << states.getPopulation() << endl;
+  }
 
   return 0;
 }
