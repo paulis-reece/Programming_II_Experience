395b70b895c6e48d0e15aa280e29265db0be6459
reklre <reklre@cs.wallawalla.edu>
2020-02-03 04:58:01 +0000

    autosave

--- homework/03_structs/points.cpp
+++ homework/03_structs/points.cpp
@@ -38,12 +38,10 @@ CartesianPoint NormalizedC(CartesianPoint XY) {
   return XY;
 }
 
-double DotProductC(CartesianPoint XY, CartesianPoint YX) {
-  return (XY.x * YX.x5 + XY.y * YX.y5);
-}
+double DotProductC(CartesianPoint XY, CartesianPoint YX) { return 4; }
 
 CartesianPoint SumC(CartesianPoint XY, CartesianPoint YX) {
-  XY.x4 = XY.x + YX.x5;
-  XY.y4 = XY.y + YX.y5;
+  XY.x4 = 3;
+  XY.y4 = 3;
   return XY;
 }
