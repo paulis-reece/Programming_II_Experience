37ac33e3f7d9cc1932441e4e0eb7f5b6137a9496
reklre <reklre@cs.wallawalla.edu>
2020-02-11 06:40:19 +0000

    autosave

--- homework/08_containers/BagOfWords.cpp
+++ homework/08_containers/BagOfWords.cpp
@@ -16,32 +16,44 @@ void BagOfWords::addWord(std::string word) { this->count.emplace(word, int()); }
 std::string BagOfWords::getTopWord() {
   int counter = 0;
   std::string topWord;
-  if (counter < this->count.at(std::string())) {
-    counter = this->count.at(std::string());
-    topWord = std::string();
+  std::map<std::string, int>::iterator iter = count.begin();
+  for (auto iter : count) {
+    if (count.count(std::string()) == 1) {
+      if (count.at(std::string()) > counter) {
+        counter = count.at(std::string());
+        topWord = std::string();
+      }
+    }
   }
   return topWord;
 }
 int BagOfWords::getUniqueWordCount() {
   int counter = 0;
-  if (this->count.count(std::string()) == 0) {
-    counter++;
+  std::map<std::string, int>::iterator iter = count.begin();
+  for (auto iter : count) {
+    if (count.count(std::string()) == 0) {
+      counter++;
+    }
   }
   return counter;
 }
 int BagOfWords::getWordCount(std::string word) {
-  int count = 0;
-  if (this->count.count(word) == 0) {
-    this->count.emplace(word, int());
-  } else {
-    count++;
+  int counter = 0;
+  std::map<std::string, int>::iterator iter = count.begin();
+  for (auto iter : count) {
+    if (count.count(word) == 1) {
+      counter = count[word]++;
+    }
   }
-  return count;
+  return counter;
 }
 void BagOfWords::printWordCount() {
   int counter = 0;
-  for (int i = 0; i < this->count.size(); i++) {
-    counter++;
+  std::map<std::string, int>::iterator iter = count.begin();
+  for (auto iter : count) {
+    if (count.count(std::string()) == 1) {
+      counter++;
+    }
   }
   std::cout << counter;
 }
