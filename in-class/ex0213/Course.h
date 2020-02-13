c346e076ecb9c18eb88dc27d5c9e468fb680e39f
reklre <reklre@cs.wallawalla.edu>
2020-02-13 21:52:23 +0000

    autosave

--- in-class/ex0213/Course.h
+++ in-class/ex0213/Course.h
@@ -1,62 +1,67 @@
 /*************************************************************************
  *
  * Checkpoint Assignment: Dynamic Memory
- * 
+ *
  * File Name:       Course.h
- * Name:            ?
+ * Name:            reklre
  * Course:          CPTR 142
- * 
+ *
  */
 #ifndef COURSE_H
 #define COURSE_H
 
+#include "Person.h"
 #include <iostream>
 #include <string>
-#include "Person.h"
+#include <vector>
 using namespace std;
 
 class Course {
 
-    private:
-        int maxSize = 0;
-        int currentEnrollment = 0;
+private:
+  int maxSize = 0;
+  int currentEnrollment = 0;
 
-        // TODO Declare the dynamic array for student roster
+  // TODO Declare the dynamic array for student roster
+  vector<Person *> studentRoster;
 
-    public:
-        // no-argument constructor
-        Course() {} 
+public:
+  // no-argument constructor
+  Course() {}
+  ~Course() {
+    for (int i = studentRoster.size() - 1; i < studentRoster.size(); i--) {
+      delete studentRoster.at(i);
+    }
+  }
+  Course(const int size) {
+    cout << "-~= Consturctor Called =~-" << endl;
+    maxSize = size;
 
-        Course(const int size) { 
-            cout << "-~= Consturctor Called =~-" << endl;
-            maxSize = size;
+    // TODO According to the number of students, create a dynamic array of
+    // Person objects
+  }
 
-            // TODO According to the number of students, create a dynamic array of Person objects
-        }
+  int getCourseSize() { return maxSize; }
 
-        int getCourseSize() {
-            return maxSize;
-        }
+  int getEnrollmentCount() { return currentEnrollment; }
 
-        int getEnrollmentCount() {
-            return currentEnrollment;
-        }
+  /**
+   * addStudent
+   * Precondition: Takes a single pointer to a Person object.
+   * Postcondition: Adds the Person pointer to the course roster.
+   */
+  void addStudent(Person *newStudent) { studentRoster.push_back(newStudent); }
 
-        /**
-         * addStudent
-         * Precondition: Takes a single pointer to a Person object.
-         * Postcondition: Adds the Person pointer to the course roster.
-         */
-        void addStudent(Person* newStudent);
-        
-        
-        /**
-         * displayRoster
-         * Precondition: None
-         * Postcondition: Prints to the console the roster of students in the course.
-         */
-        void displayRoster();
+  /**
+   * displayRoster
+   * Precondition: None
+   * Postcondition: Prints to the console the roster of students in the course.
+   */
+  void displayRoster() {
+    for (auto student : studentRoster) {
+      cout << student->getName() << endl;
+    }
+  }
 };
-
 
 #endif // COURSE_H
