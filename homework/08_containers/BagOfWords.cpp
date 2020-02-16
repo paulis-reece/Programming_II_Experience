ee6d16b29b2d668cbbec0b5b214fb407d476c897
reklre <reklre@cs.wallawalla.edu>
2020-02-16 04:05:33 +0000

    autosave

--- homework/08_containers/BagOfWords.cpp
+++ homework/08_containers/BagOfWords.cpp
@@ -24,30 +24,37 @@ void BagOfWords::addWord(std::string word) {
 std::string BagOfWords::getTopWord() {
   int counter = 0;
   std::string topWord;
-  for (int i = 0; i < count.size(); i++) {
-    if (count.count(std::string()) == 1) {
-      if (count.at(std::string()) > counter) {
-        counter = count.at(std::string());
-        topWord = count[std::string()];
+  for (auto map : count) {
+    if (count.count(map.first) == 1) {
+      if (count.at(map.first) > counter) {
+        counter = count.at(map.first);
+        topWord = map.first;
       }
+    } else {
+      addWord(map.first);
     }
   }
   return topWord;
 }
 int BagOfWords::getUniqueWordCount() {
   int counter = 0;
-  for (int i = 0; i < count.size(); i++) {
-    if (count.count(std::string()) == 0) {
+  for (auto map : count) {
+    if (count.count(map.first) == 0) {
       counter++;
+      addWord(map.first);
     }
   }
   return counter;
 }
 int BagOfWords::getWordCount(std::string word) {
   int counter = 0;
-  for (int i = 0; i < count.size(); i++) {
-    if (count.count(word) == 1) {
-      counter = count.at(word)++;
+  for (auto map : count) {
+    if (count.count(map.first) == 1) {
+      if (map.first == word) {
+        counter = count.at(word += 1);
+      }
+    } else {
+      addWord(map.first);
     }
   }
   return counter;
