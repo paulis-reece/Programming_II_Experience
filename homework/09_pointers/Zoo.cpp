a860ef8f0b0aa2f8677bd13d9f0446035f12986c
reklre <reklre@cs.wallawalla.edu>
2020-02-18 04:55:31 +0000

    autosave

--- homework/09_pointers/Zoo.cpp
+++ homework/09_pointers/Zoo.cpp
@@ -8,25 +8,36 @@
  */
 
 #include "Zoo.h"
+#include "Bee.h"
+#include "Cat.h"
+#include "Duck.h"
+#include "Snail.h"
+#include <iostream>
 // TODO Add code here
-void Zoo::addCreature(CREATURES, std::string creature) {
-
-
+void Zoo::addCreature(CREATURES creature, std::string x) {
+  if (creature == BEE) {
+    creatures.push_back(new Bee(x));
+  } else if (creature == CAT) {
+    creatures.push_back(new Cat(x));
+  } else if (creature == DUCK) {
+    creatures.push_back(new Duck(x));
+  } else if (creature == SNAIL) {
+    creatures.push_back(new Snail(x));
+  }
 }
 
 void Zoo::printCensus() {
   for (int i = 0; i < 4; i++) {
-    if (i == 0) {
-
-    } else if (i == 1) {
-
-    } else if (i == 2) {
-
-    } else if (i == 3) {
-    }
+    creatures.at(i)->getDescription();
+    creatures.at(i)->getName();
+    std::cout << std::endl;
   }
 }
 
 int Zoo::getNumberOfLegs() {
- 
+  int total = 0;
+  for (int i = 0; i < 4; i++) {
+    total += creatures.at(i)->getNumberOfLegs();
+  }
+  return total;
 }
