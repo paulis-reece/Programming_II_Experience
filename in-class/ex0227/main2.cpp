6c7adf509fd9aa2b4324dd26c1d5975fdb01da2a
reklre <reklre@cs.wallawalla.edu>
2020-02-27 21:39:24 +0000

    autosave

--- in-class/ex0227/main2.cpp
+++ in-class/ex0227/main2.cpp
@@ -11,22 +11,22 @@
  */
 
 #include <fstream>
-#include <iostream>     // for cin and cout
+#include <iostream> // for cin and cout
 #include <stdexcept>
 #include <vector>
 using namespace std;
 
-class file_not_found_error  : public runtime_error {
-    public:
-        file_not_found_error(const string &what_arg) : runtime_error(what_arg) {}
+class file_not_found_error : public runtime_error {
+public:
+  file_not_found_error(const string &what_arg) : runtime_error(what_arg) {}
 };
-class not_a_word_error      : public runtime_error {
-    public:
-        not_a_word_error    (const string &what_arg) : runtime_error(what_arg) {}
+class not_a_word_error : public runtime_error {
+public:
+  not_a_word_error(const string &what_arg) : runtime_error(what_arg) {}
 };
-class word_too_long_error   : public runtime_error {
-    public:
-        word_too_long_error (const string &what_arg) : runtime_error(what_arg) {}
+class word_too_long_error : public runtime_error {
+public:
+  word_too_long_error(const string &what_arg) : runtime_error(what_arg) {}
 };
 
 // function prototypes
@@ -34,47 +34,39 @@ class word_too_long_error   : public runtime_error {
 //    returns a success flag
 //    if successful, then lineCount has a number of lines
 //    if unsuccessful, then exception has an explanation
-bool readFile(vector<string> &words, int &errorCode, string &errorDescription);
+void readFile(vector<string> &words, int &errorCode);
 
 int main() {
-    vector<string> words;
-    int errorCode;
-    string message;
-    bool successFlag = readFile(words, errorCode, message);
-    if (successFlag) {
-        cout << "words.txt has " << words.size() << " words." << endl;
-    } else {
-        cerr << message << endl;
-    }
+  vector<string> words;
+  int errorCode;
+  string message;
+  try {
+    readFile(words, errorCode);
+  } catch (runtime_error &exception) {
+    cerr << exception.what() << endl;
+  }
 }
 
-bool readFile(vector<string> &words, int &errorCode, string &errorDescription) {
-    ifstream inFS;   // Input file stream
-    errorCode = 0;
+void readFile(vector<string> &words, int &errorCode) {
+  ifstream inFS; // Input file stream
+  errorCode = 0;
 
-    inFS.open("words.txt");
-    if (!inFS.is_open()) {
-        errorCode = 1;
-        errorDescription = "Unable to open words.cpp!";
-        return false;
+  inFS.open("words.txt");
+  if (!inFS.is_open()) {
+    throw file_not_found_error("Unable to open words.cpp!");
+  }
+  while (!inFS.eof()) {
+    string line;
+    getline(inFS, line);
+    if (!inFS.fail() && line.size() > 0) {
+      if (isdigit(line.at(0))) {
+        throw not_a_word_error("Found a digit");
+      }
+      if (line.size() > 20) {
+        throw word_too_long_error("Words too long");
+      }
+      words.push_back(line);
     }
-    while (!inFS.eof()) {
-        string line;
-        getline(inFS, line);
-        if (!inFS.fail() && line.size() > 0) {
-            if (isdigit(line.at(0))) {
-                errorCode = 2;
-                errorDescription = "Found a digit!";
-                return false;
-            }
-            if (line.size() > 20) {
-                errorCode = 3;
-                errorDescription = "Word is too long!";
-                return false;
-            }
-            words.push_back(line);
-        }
-    }
-    inFS.close();
-    return true;
+  }
+  inFS.close();
 }
