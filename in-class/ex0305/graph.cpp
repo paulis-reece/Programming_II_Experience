a7381487846391a98215f505585a44d9e4ed9a57
James Foster <github@jgfoster.net>
2020-03-04 22:27:37 -0800

    Update exercises for content and format.

--- in-class/ex0305/graph.cpp
+++ in-class/ex0305/graph.cpp
@@ -1,56 +1,50 @@
 /*************************************************************************
  *
- * Exercise: Graph Structure
+ * Exercise: Tree Structure
  *
- * File Name:   graph.cpp
+ * File Name:   tree.cpp
  * Username:  	?
  * Username:  	?
  * Course:      CPTR 142
  *
  */
 
-#include <iostream>     // for cin and cout
+#include <iostream> // for cin and cout
 #include <vector>
 using namespace std;
 
 class Node {
-    public:
-        Node() { this->value = rand() % 100; }
-        int getValue() { return value; }
-        void addLinkTo(Node* node);
-        void print();
-    private:
-        int value;
-        vector<Node*> links;
+public:
+  Node() { this->value = rand() % 100; }
+  int getValue() { return value; }
+  void addLevels(int levels);
+  void print(string indent);
+
+private:
+  int value;
+  vector<Node *> children;
 };
 
-void Node::addLinkTo(Node* node) {
-    for (int i = 0; i < links.size(); ++i) {
-        if (links.at(i) == node) {
-            return;
-        }
-        links.push_back(node);
-        node->addLinkTo(this);
-    }
+void Node::addLevels(int levels) {
+  if (levels == 0) {
+    return;
+  }
+  int childrenCount = rand() % 3; // allow zero to two children
+  for (int i = 0; i <= childrenCount; ++i) {
+    Node *child = new Node;
+    child->addLevels(rand() % levels); //
+    children.push_back(child);
+  }
 }
 
-void Node::print() {
+void Node::print(string indent) {
+  // TODO: Print this node's value on a line and then print each child
 }
 
 int main() {
-    srand(0);
-    vector<Node*> nodes;
-    for (int i = 0; i < 4; ++i) {
-        nodes.push_back(new Node);
-    }
-    for (int i = 0; i < nodes.size(); ++i) {
-        for (int j = i + 1; j < nodes.size(); ++j) {
-            cout << i << " " << j << " " << nodes.size() << endl;
-            nodes.at(i)->addLinkTo(nodes.at(j));
-        }
-    }
-    for (int i = 0; i < nodes.size(); ++i) {
-        nodes.at(i)->print();
-    }
-    return 0;
+  srand(0);
+  Node *root = new Node;
+  root->addLevels(3);
+  root->print("");
+  return 0;
 }
--- in-class/ex0305/tree.cpp
+++ in-class/ex0305/tree.cpp
@@ -9,39 +9,42 @@
  *
  */
 
-#include <iostream>     // for cin and cout
+#include <iostream> // for cin and cout
 #include <vector>
 using namespace std;
 
 class Node {
-    public:
-        Node() { this->value = rand() % 100; }
-        int getValue() { return value; }
-        void addLevels(int levels);
-        void print(string indent);
-    private:
-        int value;
-        vector<Node *> children;
+public:
+  Node() { this->value = rand() % 100; }
+  int getValue() { return value; }
+  void addLevels(int levels);
+  void print(string indent);
+
+private:
+  int value;
+  vector<Node *> children;
 };
 
 void Node::addLevels(int levels) {
-    if (levels == 0) {
-        return;
-    }
-    int childrenCount = rand() % 4; // allow zero to three children
-    for (int i = 0; i <= childrenCount; ++i) {
-        Node* child = new Node;
-        child->addLevels(rand() % levels);   //
-        children.push_back(child);
-    }
+  if (levels == 0) {
+    return;
+  }
+  int childrenCount = rand() % 4; // allow zero to three children
+  for (int i = 0; i <= childrenCount; ++i) {
+    Node *child = new Node;
+    child->addLevels(rand() % levels); //
+    children.push_back(child);
+  }
 }
 
 void Node::print(string indent) {
+  // TODO: Print this node's value on a line and then print each child
 }
 
 int main() {
-    srand(0);
-    Node* root = new Node;
-    root->addLevels(4);
-    return 0;
+  srand(0);
+  Node *root = new Node;
+  root->addLevels(4);
+  root->print("");
+  return 0;
 }
