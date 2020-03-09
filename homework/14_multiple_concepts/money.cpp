c980707e5f34a19db790ebbe582b290594548498
reklre <reklre@cs.wallawalla.edu>
2020-03-09 22:22:28 +0000

    autosave

--- homework/14_multiple_concepts/money.cpp
+++ homework/14_multiple_concepts/money.cpp
@@ -3,13 +3,30 @@
  * Homework: Money
  *
  * File Name:   money.cpp
- * Username:  	?
+ * Username:  	Reece Reklai
  * Course:      CPTR 142
- * Date:        ?
+ * Date:        3/9/2020
  *
  */
+#include "money.h"
+#include <stdexcept>
 #include <string>
 using namespace std;
 
 // throws a runtime_error if the input string is not properly formatted
-void verifyIsMoney(string inputString) {}
+void verifyIsMoney(string inputString) {
+  int index = 0;
+  if (inputString.length() == 0) {
+    return;
+  } else {
+    if (inputString.at(0) != '$') {
+      throw runtime_error("the input string is not properly formatted");
+    } else if(inputString.at(1)){} else {
+      if (inputString.at(index) == ',') {
+
+      } else {
+
+      }
+    }
+  }
+}
