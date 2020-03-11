c92f2ede2c94fefbb5c698f9cde50ef953114a32
reklre <reklre@cs.wallawalla.edu>
2020-03-11 20:52:35 +0000

    autosave

--- homework/14_multiple_concepts/money.cpp
+++ homework/14_multiple_concepts/money.cpp
@@ -16,10 +16,29 @@ using namespace std;
 // throws a runtime_error if the input string is not properly formatted
 void state4(string input, int index) {}
 
-void state1(string input, int index) {}
+void state1(string input, int index) {
+  if (input.at(index) == ',') {
+    state4(input, index++);
+  } else if (index == 4) {
+    if (input.at(index) != '\0') {
+      throw runtime_error("Testing is NOT money");
+    } else {
+      return;
+    }
+  } else if (input.at(index) == '\0') {
+    return;
+  } else {
+    state1(input, index++);
+  }
+}
 
 void state0(string input, int index) {
-    
+  if (ispunct(input.at(index)) == true && isdigit(input.at(index)) == false &&
+      input.at(index) == '0' && input.at(index) == '\0') {
+    throw runtime_error("Testing is NOT money");
+  } else {
+    state1(input, index++);
+  }
 }
 void verifyIsMoney(string inputString) {
   int index = 0;
