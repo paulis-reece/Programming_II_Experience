760a6a81a88bf1424805f5deda9f572fbbe3435c
reklre <reklre@cs.wallawalla.edu>
2020-03-12 02:23:37 +0000

    autosave

--- homework/14_multiple_concepts/money.cpp
+++ homework/14_multiple_concepts/money.cpp
@@ -34,7 +34,9 @@ void state4(string input, int index, int counter) {
 
 void state1(string input, int index) {
   int counter = 0;
-  if (index == 4) {
+  if (input.at(index) == '\0') {
+    return;
+  } else if (index == 4) {
     if (input.at(index) != ',') {
       throw runtime_error("Testing is NOT money");
     } else {
