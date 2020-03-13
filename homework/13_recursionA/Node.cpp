46b016300a48d2def1205854d249c04565cf66ba
reklre <reklre@cs.wallawalla.edu>
2020-03-13 08:08:38 +0000

    autosave

--- homework/13_recursionA/Node.cpp
+++ homework/13_recursionA/Node.cpp
@@ -41,16 +41,15 @@ void Node::print() {
 }
 
 int Node::costOfPath(deque<Node *> path) {
-  Node *Nptr;
   // remove the current node from the front of the path
   path.pop_front();
   // if nothing more, then return cost of zero
   if (path.size() == 0) {
     return 0;
+  } else {
+    // return cost to next node plus cost from there to end
+    return this->linkAndCostPairs.at(path.front()) + costOfPath(path);
   }
-  // return cost to next node plus cost from there to end
-  Nptr = path.front();
-  return this->linkAndCostPairs.at(Nptr) + costOfPath(path);
 }
 
 void Node::printPath(deque<Node *> path) {
@@ -65,27 +64,18 @@ void Node::printPath(deque<Node *> path) {
     return;
   }
   // print a dash, then recurse to print remaining path
+  cout << "-";
 }
 
 // add to a vector of paths leading back home
 void Node::findPaths(deque<Node *> currentPath, vector<deque<Node *>> &allPaths,
                      string indent) {
-  cout << indent << "findPaths() - starting at " << label << endl;
-
+  // cout << indent << "findPaths() - starting at " << label << endl;
   // BASE CASE 1: if `this` is not home but part of the current path, do nothing
-  bool home = false;
-  for (auto each : currentPath) {
-    if (this == each) {
-      home = true;
-    }
-    if (this != currentPath.front() && home) {
-      return;
-    }
-  }
   // we are visiting a new node, so add `this` to end of the current path
   currentPath.push_back(this);
   // BASE CASE 2: if we left home and got back, add currentPath to allPaths
-  if (currentPath.front() == currentPath.back()) {
+  if (currentPath.front()->getLabel() == currentPath.back()->getLabel()) {
     allPaths.push_back(currentPath);
   }
   // RECURSION: visit each child (link) and add any discovered paths
