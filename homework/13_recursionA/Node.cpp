c0b37f87cb26b48aea27d7c886c466d7fe4835b4
reklre <reklre@cs.wallawalla.edu>
2020-03-15 03:44:50 +0000

    autosave

--- homework/13_recursionA/Node.cpp
+++ homework/13_recursionA/Node.cpp
@@ -85,15 +85,20 @@ void Node::findPaths(deque<Node *> currentPath, vector<deque<Node *>> &allPaths,
                      string indent) {
   // cout << indent << "findPaths() - starting at " << label << endl;
   map<Node *, int>::iterator loopMap;
-  char node;
   // BASE CASE 1: if `this` is not home but part of the current path, do nothing
-  if (currentPath.front()->getLabel() == 'A') {
-    return;
+  bool home = false;
+  for (auto each : currentPath) {
+    if (this->getLabel() == each->getLabel()) {
+      home = true;
+    }
+    if (this == currentPath.front() && home) {
+      return;
+    }
   }
   // we are visiting a new node, so add `this` to end of the current path
   currentPath.push_back(this);
   // BASE CASE 2: if we left home and got back, add currentPath to allPaths
-  if (currentPath.front()->getLabel() == currentPath.back()->getLabel()) {
+  if (currentPath.front()->getLabel() == 'A') {
     allPaths.push_back(currentPath);
   }
   // RECURSION: visit each child (link) and add any discovered paths
